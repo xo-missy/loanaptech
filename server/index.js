7690d49c564694279ee655d704ca06585739e797
OLADIPO KEHINDE <oladipomarylyn23@gmail.com>
2026-05-04 11:13:26 +0100

    changes in index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,8 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'https://loanaptech.vercel.app/'],
+  origin: ['http://localhost:5173', 
+    'https://loanaptech.vercel.app/'],
   credentials: true
 }));
 
